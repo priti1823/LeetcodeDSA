fba1125a303e93204e78605ea779cc7676760f8f
Priti Srivastava <pritisrivastava1823@gmail.com>
2026-09-06 14:11:25 +0530

    [1827] [Time Beats: 5.91%] [Memory Beats: 28.26%] - LeetPush

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-Minimum-Operations-to-Make-the-Array-Increasing.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int op=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]<=nums[i-1])
+            {while(nums[i]<=nums[i-1])
+            {
+                nums[i]=nums[i]+1;
+                 op++;
+            }
+
+            }
+        }
+        return op;
+        
+    }
+};
