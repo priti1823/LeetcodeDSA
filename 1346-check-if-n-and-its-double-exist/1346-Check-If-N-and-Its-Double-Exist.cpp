885fe161f8438ca45c9d083307c36101d3121403
Priti Srivastava <pritisrivastava1823@gmail.com>
2026-09-06 12:44:41 +0530

    [1346] [Time Beats: 100.00%] [Memory Beats: 49.68%] - LeetPush

---
+++ 1346-check-if-n-and-its-double-exist/1346-Check-If-N-and-Its-Double-Exist.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        for(int i=0;i<arr.size();i++)
+        {
+            for(int j=0;j<arr.size();j++)
+            {  if(j!=i)
+               {  if(arr[i]==2*arr[j])
+                {
+                return true;
+                 }
+
+                }
+
+            }
+        }
+        return false;
+        
+    }
+};
