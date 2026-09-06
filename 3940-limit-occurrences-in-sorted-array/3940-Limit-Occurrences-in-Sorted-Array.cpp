0d6dc1932a42ff19ac287620edf60914d40594be
Priti Srivastava <pritisrivastava1823@gmail.com>
2026-09-06 17:22:10 +0530

    [3940] [Time Beats: 18.88%] [Memory Beats: 67.95%] - LeetPush

---
+++ 3940-limit-occurrences-in-sorted-array/3940-Limit-Occurrences-in-Sorted-Array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> limitOccurrences(vector<int>& nums, int k) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++)
+        {  int count=0;
+          if(i==0||nums[i]!=nums[i-1])
+          {
+            for(int j=0;j<nums.size();j++)
+            {
+                if(nums[i]==nums[j])
+                count++;
+
+            }
+            if(count>k)
+             count=k;
+             while(count>0)
+             {
+                ans.push_back(nums[i]);
+                count--;
+             }
+
+          }
+
+        }
+       return ans; 
+    }
+};
